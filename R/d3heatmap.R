f5d90e03d23013c47e5bb13aade9f3fe3b84ae51
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2018-02-08 22:50:50 +0000

    Ensure names exist

--- R/d3heatmap.R
+++ R/d3heatmap.R
@@ -421,8 +421,10 @@ d3heatmap <- function(x,
   side_colors <- list(
     col_cols=col_cols,
     row_cols=row_cols,
-    rowcolor_colnames = rowcolor_colnames,
-    colcolor_colnames = colcolor_colnames,
+    rowcolor_colnames = if (exists("rowcolor_colnames", inherits = FALSE) rowcolor_colnames
+      else NULL,
+    colcolor_colnames =  if (exists("colcolor_colnames", inherits = FALSE) colcolor_colnames
+      else NULL,
     rowcolors = if (!is.null(RowSideFactors)) RowSideFactors else NULL,
     colcolors = if (!is.null(ColSideFactors)) ColSideFactors else NULL
   )
